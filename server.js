775777f7eb6ee148d2ba77dbdeca41a7a3b08be6
Derek Bennett <bnnder@netscape.net>
2019-12-10 02:34:38 -0800

    created a Procfile for heroku to the server.js file and added the heroku listening process.env.PORT

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 // Required npm package
 var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080; // heroku used port with the process.env.Port when ran locally use port 8080
+
+app.use(express.static(__dirname));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./app/routing/apiRoutes.js")(app);  
+require("./app/routing/htmlRoutes.js")(app);
+
+app.listen(port, function() {
+	console.log("App listening on port: " + port);
+});
